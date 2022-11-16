1e892763f09e46575050dfc57336c0bd876558d1
Aulia Akbar Khoiruddin <92700547+auliakbark@users.noreply.github.com>
2022-11-16 07:48:49 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         User::create([
             'name' => 'Admin',
-            'email' => 'admin123@gmail.com',
+            'email' => 'admin@gmail.com',
             'password' => Hash::make('12345678'),
             'role' => 'Admin',
             'is_aktif' => '1',
